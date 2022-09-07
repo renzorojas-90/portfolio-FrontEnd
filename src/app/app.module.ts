00459e754debe37dc61c77bb623859caf3d44f6b
Renzo <renzorojasqarg@hotmail.com>
2022-09-07 01:16:42 -0300

    creando los componentes de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { CuerpoComponent } from './cuerpo/cuerpo.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    CuerpoComponent,
+    FooterComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule,
